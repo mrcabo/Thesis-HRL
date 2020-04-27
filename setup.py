11617b0cdafe056f2b9cf6161c5469f8654beb5d
mrcabo <dcgdiego@gmail.com>
2020-04-27 17:12:40 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="Thesis-HRL",
-    version="0.1",
-    packages=find_packages(),
-    scripts=["say_hello.py"],
-
-    install_requires=['gym', 'numpy'],
+    version="0.0.1",
+    install_requires=['gym', 'numpy', 'torch', 'matplotlib'],
     include_package_data=True,
 
     # metadata to display on PyPI
     author="Diego Cabo Golvano",
     author_email="dcgdiego@gmail.com",
-    description="This is an Example Package",  # TODO
-    keywords="hello world example examples",
-    url="https://github.com/mrcabo/Thesis-HRL",  # project home page, if any
+    description="This is an environment for RL purposes",  # TODO
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Reinforcement Learning environment",
+    url="https://github.com/mrcabo/Household-env.git",  # project home page, if any
     project_urls={
-        "Bug Tracker": "https://bugs.example.com/HelloWorld/",
-        "Documentation": "https://docs.example.com/HelloWorld/",
-        "Source Code": "https://github.com/mrcabo/Thesis-HRL",
+        "Source Code": "https://github.com/mrcabo/Household-env.git",
     },
+    packages=setuptools.find_packages(),
     classifiers=[
-        "License :: OSI Approved :: Python Software Foundation License"
-    ]
-
-    # could also include long_description, download_url, etc.
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
