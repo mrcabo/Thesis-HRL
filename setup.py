e8b50f821cb049a707f55662cce0d978b2e66a7c
mrcabo <dcgdiego@gmail.com>
2020-04-28 13:12:13 +0200

    Updated setup.py so it installs everything.

--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Thesis-HRL",
+    name="thesishrl",
     version="0.0.1",
-    install_requires=['gym', 'numpy', 'torch', 'matplotlib'],
-    include_package_data=True,
-
+    packages=setuptools.find_packages(),
+    install_requires=['gym', 'householdenv', 'torch', 'matplotlib'],
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.json"]
+    },
     # metadata to display on PyPI
     author="Diego Cabo Golvano",
     author_email="dcgdiego@gmail.com",
-    description="This is an environment for RL purposes",  # TODO
+    description="This is my thesis work on HRL",  # TODO
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="Reinforcement Learning environment",
-    url="https://github.com/mrcabo/Household-env.git",  # project home page, if any
+    url="https://github.com/mrcabo/Thesis-HRL.git",  # project home page, if any
     project_urls={
-        "Source Code": "https://github.com/mrcabo/Household-env.git",
+        "Source Code": "https://github.com/mrcabo/Thesis-HRL.git",
     },
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
