cca0eea8cfebf616408947bb5348b6636cc94e8a
mrcabo <dcgdiego@gmail.com>
2020-10-06 19:48:36 +0200

    Testing learning new task

--- thesis_hrl/train_new_task.py
+++ thesis_hrl/train_new_task.py
@@ -7,7 +7,7 @@ from household_env.envs.house_env import Tasks
 from thesis_hrl.config import CONF_DIR
 from thesis_hrl.model import HRLDQN
 from thesis_hrl.utils import parse_arguments
-from thesis_hrl.training import train
+from thesis_hrl.train_2 import train
 
 if __name__ == '__main__':
     args = parse_arguments()  # vars(args)  # Turns it into a dictionary.
@@ -22,10 +22,9 @@ if __name__ == '__main__':
     if not results_path.exists():
         Path.mkdir(results_path, parents=True)
 
-    # NOTE: pre-trained weights should be copied to results_path
-
     env = gym.make('household_env:Household-v0')
-    tasks_list = [Tasks.CLEAN_STOVE]
+    tasks_list = [Tasks.MAKE_PASTA, Tasks.MAKE_TEA]
+
     my_model = HRLDQN(env.observation_space.shape[0], env.action_space.n, **hyperparam)
     my_model.print_model()
 
@@ -34,7 +33,6 @@ if __name__ == '__main__':
         raise Exception("The necessary argument --weights must indicate the name of a real directory that contains "
                         "the pre-trained weights.")
     my_model.load_model(results_path.parent / args.weights)
-    my_model.prev_trained = [Tasks.MAKE_TEA, Tasks.MAKE_SOUP]
     # Train model for the new task
     train(env, my_model, tasks_list, results_path, **hyperparam)
 
--- thesis_hrl/train_new_task_remind.py
+++ thesis_hrl/train_new_task_remind.py
@@ -12,81 +12,52 @@ from thesis_hrl.utils import parse_arguments, normalize_values
 from thesis_hrl.training import train, plot_and_save
 
 
-def train_new_task(env, model, task_list, results_path, cycle_rewards, **kwargs):
-    filename_ep_reward = results_path / 'Episode rewards.png'
-    filename_cum_reward = results_path / 'Cumulative rewards.png'
-    W = kwargs.get('warmup_period')
-    J = kwargs.get('joint_period')
-
-    for i_cycle in range(kwargs.get('remind_freq')):
-        # Sample a task and initialize environment
-        state = env.reset()
-        chosen_task = random.choice(task_list)
-        print(f"Chosen task: {chosen_task.name}")  # DEBUG
-        state = env.set_current_task(chosen_task)
-        state = normalize_values(torch.tensor(state, dtype=torch.float, device=model.device))
-        cycle_reward = 0
-        # Reset master policy
-        model.master_policy.reset()
-        model.master_ER.reset()
-
-        # Warmup period
-        for w in range(W):
-            master_action = model.master_policy.select_action(state)  # Chooses a policy
-            primitive_action = model.sub_policies[master_action.item()].policy_net(state).max(0)[1].view(1, 1)
-            next_state, reward, done, _ = env.step(primitive_action.item())
-            # print(f"Primitive action taken: {policy_action.item()}")
-            cycle_reward += reward
-            next_state = normalize_values(torch.tensor(next_state, dtype=torch.float, device=model.device))
-            reward = torch.tensor([reward], dtype=torch.float, device=model.device)
-            done = torch.tensor([done], dtype=torch.bool, device=model.device)
-            model.master_ER.push(state.unsqueeze(0), master_action, next_state.unsqueeze(0), reward, done)
-            state = next_state
-
-            model.optimize_master()
-            # env.render()
-            if done:
-                # This ensures that W updates will be performed always.
-                state = env.reset()
-                state = env.set_current_task(chosen_task)
-                state = normalize_values(torch.tensor(state, dtype=torch.float, device=model.device))
-            # Update the target network, copying all weights and biases in DQN
-            if model.master_policy.steps_done % model.M_TARGET_UPDATE == 0:
+def train_new_task(env, model, task_list, **kwargs):
+    # Sample a task and initialize environment
+    chosen_task = random.choice(task_list)
+    print(f"Chosen task: {chosen_task.name}")  # DEBUG
+    model.master_policy.reset()
+    # Train on ERs
+    if len(model.master_ERs[chosen_task.name]) > 0:
+        for i in range(kwargs.get('train_iters_M')):
+            model.optimize_master(model.master_ERs[chosen_task.name])
+            model.master_policy.updates_done += 1
+            if model.master_policy.updates_done % model.M_TARGET_UPDATE == 0:
                 model.master_policy.target_net.load_state_dict(model.master_policy.policy_net.state_dict())
-
-        # Joint update period
-        for j in range(J):
-            master_action = model.master_policy.select_action(state)  # Chooses a policy
-            primitive_action = model.sub_policies[master_action.item()].select_action(state)
-            next_state, reward, done, _ = env.step(primitive_action.item())
-            # print(f"Primitive action taken: {policy_action.item()}")
-            cycle_reward += reward
-            next_state = normalize_values(torch.tensor(next_state, dtype=torch.float, device=model.device))
-            reward = torch.tensor([reward], dtype=torch.float, device=model.device)
-            done = torch.tensor([done], dtype=torch.bool, device=model.device)
-            model.master_ER.push(state.unsqueeze(0), master_action, next_state.unsqueeze(0), reward, done)
-            model.task_ERs[chosen_task.name].push(state.unsqueeze(0), primitive_action, next_state.unsqueeze(0), reward,
-                                                  done)
-
-            state = next_state
-
-            model.optimize_master()
-            model.optimize_sub(model.task_ERs[chosen_task.name], master_action.item())
-            if done:
-                state = env.reset()
-                state = env.set_current_task(chosen_task)
-                state = normalize_values(torch.tensor(state, dtype=torch.float, device=model.device))
-            # Update the target network, copying all weights and biases in DQN
-            if model.master_policy.steps_done % model.M_TARGET_UPDATE == 0:
-                model.master_policy.target_net.load_state_dict(model.master_policy.policy_net.state_dict())
+        for i in range(kwargs.get('train_iters')):
+            idx = model.master_ERs[chosen_task.name].sample(1)[0].action.item()
+            model.optimize_sub(model.task_ERs[chosen_task.name], idx)
+            model.sub_policies[idx].updates_done += 1
             for policy in model.sub_policies:
-                if policy.steps_done % model.S_TARGET_UPDATE == 0:
+                if policy.updates_done % model.S_TARGET_UPDATE == 0:
                     policy.target_net.load_state_dict(policy.policy_net.state_dict())
+    # Get more experiences
+    env.reset()
+    state = env.set_current_task(chosen_task)
+    state = normalize_values(torch.tensor(state, dtype=torch.float, device=model.device))
+    cycle_reward = 0
+    for t in range(kwargs.get('collect_exp')):
+        master_action = model.master_policy.select_action(state)  # Chooses a policy
+        primitive_action = model.sub_policies[master_action.item()].select_action(state)
+        next_state, reward, done, _ = env.step(primitive_action.item())
+        # print(f"Primitive action taken: {policy_action.item()}")
+        cycle_reward += reward
+        next_state = normalize_values(torch.tensor(next_state, dtype=torch.float, device=model.device))
+        reward = torch.tensor([reward], dtype=torch.float, device=model.device)
+        done = torch.tensor([done], dtype=torch.bool, device=model.device)
+        model.master_ERs[chosen_task.name].push(state.unsqueeze(0), master_action,
+                                                next_state.unsqueeze(0), reward, done)
+        if not model.master_policy.rand_action:
+            # Only adding experience to replay if master actions are following the optimal policy
+            model.task_ERs[chosen_task.name].push(state.unsqueeze(0), primitive_action,
+                                                  next_state.unsqueeze(0), reward, done)
+        state = next_state
+        if done:
+            state = env.reset()
+            state = env.set_current_task(chosen_task)
+            state = normalize_values(torch.tensor(state, dtype=torch.float, device=model.device))
 
-        cycle_rewards.append(cycle_reward)
-        if i_cycle % 10 == 0:
-            print(f"Cycle {i_cycle}")
-            plot_and_save(model, cycle_rewards, results_path, filename_ep_reward, filename_cum_reward)
+    return cycle_reward
 
 
 def remind_old_tasks(model):
@@ -98,9 +69,18 @@ def train(env, model, task_list, results_path, **kwargs):
     filename_ep_reward = results_path / 'Episode rewards.png'
     filename_cum_reward = results_path / 'Cumulative rewards.png'
     cycle_rewards = []
-    for i_cycle in range(kwargs.get('num_cycles')):
-        train_new_task(env, model, task_list, results_path, cycle_rewards, **kwargs)
-        remind_old_tasks(model)
+    remind_freq = kwargs.get('remind_freq')
+    for i_cycle in range(kwargs.get('num_episodes')):
+        # Train on new task
+        cycle_reward = train_new_task(env, model, task_list, **kwargs)
+        # Remind old tasks every n-th cycles
+        if i_cycle % remind_freq == 0:
+            remind_old_tasks(model)
+        cycle_rewards.append(cycle_reward)
+        # Plot and save every 500 cycles
+        if i_cycle % 500 == 0:
+            print(f"Cycle {i_cycle}")
+            plot_and_save(model, cycle_rewards, results_path, filename_ep_reward, filename_cum_reward)
     cum_r = plot_and_save(model, cycle_rewards, results_path, filename_ep_reward, filename_cum_reward)
     model.save_task_memories(results_path)
     print('Training complete')
@@ -123,7 +103,7 @@ if __name__ == '__main__':
     # NOTE: pre-trained weights should be copied to results_path
 
     env = gym.make('household_env:Household-v0')
-    tasks_list = [Tasks.CLEAN_STOVE]
+    tasks_list = [Tasks.MAKE_SOUP]
     my_model = HRLDQN(env.observation_space.shape[0], env.action_space.n, **hyperparam)
     my_model.print_model()
 
@@ -133,7 +113,7 @@ if __name__ == '__main__':
                         "the pre-trained weights.")
     my_model.load_model(results_path.parent / args.weights)
     my_model.load_task_memories(results_path.parent / args.weights)
-    my_model.prev_trained = [Tasks.MAKE_TEA, Tasks.MAKE_SOUP]
+    my_model.prev_trained = [Tasks.MAKE_TEA, Tasks.MAKE_PASTA]
     # Train model for the new task
     train(env, my_model, tasks_list, results_path, **hyperparam)
 
