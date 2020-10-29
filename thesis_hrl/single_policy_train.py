22e215a741c8703622345c4f2b0d86c23c6ddbb4
mrcabo <dcgdiego@gmail.com>
2020-10-29 16:25:41 +0100

    Trying train all with bigger net

--- thesis_hrl/single_policy_train.py
+++ thesis_hrl/single_policy_train.py
@@ -85,7 +85,8 @@ if __name__ == '__main__':
         Path.mkdir(results_path, parents=True)
 
     env = gym.make('household_env:Household-v0')
-    tasks_list = [Tasks.MAKE_PASTA, Tasks.MAKE_TEA, Tasks.MAKE_OMELETTE]
+    tasks_list = [Tasks.MAKE_PASTA, Tasks.MAKE_TEA, Tasks.MAKE_SOUP, Tasks.MAKE_OMELETTE, Tasks.CLEAN_STOVE,
+                  Tasks.MAKE_PANCAKES]
     env.set_current_task(tasks_list[0])
 
     my_model = QLearning(env.observation_space.shape[0], env.action_space.n, **hyperparam)
